bc7cabe1f1e972154b9bac6c4672b2b5785300f0
jw <protectingjw@naver.com>
2020-07-23 18:50:25 +0900

    commit

--- src/baekjoon_train/SangenNalde.java
+++ src/baekjoon_train/SangenNalde.java
@@ -8,12 +8,29 @@ public class SangenNalde {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int ham = 0;
-		int bev = 0;
+		Scanner scanner = new Scanner(System.in);
+		
+
+		int bugU =0;
+		int bugM =0;
+		int bugL =0;
+		
+		int temp = 0;
 		int sum = 0;
 		int set = (sum-50);
 		
 		
+		
+		for(int i=0; i<5; i++) {
+			temp = scanner.nextInt();	
+		}
+		while(temp >=  100 && temp <=2000)
+		{
+			
+		}
+		
+		
+
 		
 	}
 
